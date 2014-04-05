8b3d4604bb180f8c89c46b6953cb1b07a90027fe
Andrew Butterfield <andrewcbutterfield@gmail.com>
2014-04-05 15:30:39 -0600

    Redirects to my tickets now for using

--- locallib.php
+++ locallib.php
@@ -779,11 +779,11 @@ function tracker_searchforissues(&$tracker, $cmid){
     $success = tracker_setsearchcookies($fields);
     
     if ($success){
-        //if ($tracker->supportmode == 'bugtracker'){
+        if ($tracker->supportmode == 'bugtracker'){
             redirect ("view.php?id={$cmid}&amp;view=view&amp;screen=browse");
-        //} else {
-        //    redirect("view.php?id={$cmid}&amp;view=view&amp;screen=mytickets");
-		//}
+        } else {
+            redirect("view.php?id={$cmid}&amp;view=view&amp;screen=mytickets");
+		}
     } else {
         print_error('errorcookie', 'tracker', '', $cookie);
     }
